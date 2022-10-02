0517bf72b1b3b21ef717b9d983033c6ae6b4ab7a
Colin Percival <cperciva@freebsd.org>
2022-10-02 10:42:41 -0700

    pvh/arch-x86_64: Write start_info to guest memory
    Fill the hvm_start_info and related structures as specified
    in the PVH boot protocol. Write the data structures to guest
    memory at the GPA that will be stored in %rbx when the guest starts.
    
    Signed-off-by: Colin Percival <cperciva@freebsd.org>
    Co-authored-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>

--- src/vmm/src/builder.rs
+++ src/vmm/src/builder.rs
@@ -858,6 +858,7 @@ pub fn configure_system_for_boot(
             cmdline_size,
             initrd,
             vcpus.len() as u8,
+            entry_point.protocol,
         )
         .map_err(ConfigureSystem)?;
     }
